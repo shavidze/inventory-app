79cccb2b1ea04cecd9c0804eda56b672e965c366
Saba Shavidze <sabashavidze@gmail.com>
2023-07-06 12:25:47 +0400

    create a user table with schema

--- server/db/migrations/20230705101530_initial.js
+++ server/db/migrations/20230705101530_initial.js
@@ -1,9 +1,18 @@
+/* eslint-disable no-tabs */
+const tableNames = require('../../src/utils/constants/tableNames');
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-
+exports.up = async (knex) => {
+  await knex.schema.createTable(tableNames.user, (table) => {
+    /**
+	 * it creates a columnt with an integer data type that
+	 * authomatically increments by 1 for each row inserted into the table
+	 */
+    table.increments().notNullable();
+  });
 };
 
 /**
