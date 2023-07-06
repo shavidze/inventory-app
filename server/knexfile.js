90a7e8f464758ee363d89f8722081fc79f14f391
Saba Shavidze <sabashavidze@gmail.com>
2023-07-06 13:36:22 +0400

    New: migration -add default columns function

--- server/knexfile.js
+++ server/knexfile.js
@@ -8,7 +8,6 @@ module.exports = {
       database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
-      socketPath: '/var/run/postgresql',
     },
     migrations: {
       directory: './db/migrations',
