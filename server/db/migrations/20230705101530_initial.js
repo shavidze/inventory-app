9203bd4b5f58259788bac2d4a8e34c32f3a1c38f
Saba Shavidze <sabashavidze@gmail.com>
2023-07-06 13:26:13 +0400

    New: migration -add default columns function

--- server/db/migrations/20230705101530_initial.js
+++ server/db/migrations/20230705101530_initial.js
@@ -3,6 +3,17 @@ const Knex = require('knex');
 const tableNames = require('../../src/utils/constants/tableNames');
 
 /**
+ *
+ * @param {} table
+ * @returns table with created_at, updated_at, deleted_at columns
+ */
+function addDefaultColumns(table) {
+  table.timestamps(false, true);
+  table
+    .datetime('deleted_at', { precision: 6 });
+}
+
+/**
  * @param { Knex } knex
  * @returns { Promise<void> }
  */
@@ -13,6 +24,7 @@ exports.up = async (knex) => {
     table.string('name').notNullable();
     table.text('password', 64).notNullable();
     table.datetime('last_login');
+    addDefaultColumns(table);
   });
 };
 
