7504cb853460a872e48cf1df7cc42aff3878e2e0
Saba Shavidze <sabashavidze@gmail.com>
2023-07-07 16:45:03 +0400

    seed first user into user table

--- server/db/seed/initial.js
+++ server/db/seed/initial.js
@@ -1,13 +1,20 @@
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
+const orderedTableNames = require('../../src/utils/constants/sortedTableNames');
+const tableNames = require('../../src/utils/constants/tableNames');
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del();
-  await knex('table_name').insert([
-    { id: 1, colName: 'rowValue1' },
-    { id: 2, colName: 'rowValue2' },
-    { id: 3, colName: 'rowValue3' },
-  ]);
+  await Promise.all(orderedTableNames.map((table_name) => knex(table_name).del()));
+  const password = uuidv4().replace(/-/g, '');
+  const user = {
+    email: 'sabashavidze@gmail.com',
+    name: 'Saba',
+    password: await bcrypt.hash(password, 12),
+  };
+  const [createdUser] = await knex(tableNames.user).insert(user).returning('*');
+
+  console.log('User created', { password }, createdUser);
 };
---
+++ server/src/utils/constants/sortedTableNames.js
@@ -0,0 +1,11 @@
+const tableNames = require('./tableNames');
+
+module.exports = [
+  tableNames.item_type,
+  tableNames.company,
+  tableNames.address,
+  tableNames.state,
+  tableNames.country,
+  tableNames.shape,
+  tableNames.location,
+];
