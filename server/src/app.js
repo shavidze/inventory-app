107537cc041243808b8953bd3a201b66f5a2a797
Saba Shavidze <sabashavidze@gmail.com>
2023-07-10 10:49:18 +0400

    NEW: place middlewares between req/res

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const app = express();
+app.use(morgan('tiny'));
+app.use(compression());
+app.use(helmet());
 
 module.exports = app;
