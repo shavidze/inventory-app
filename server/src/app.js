a36dfd13ac02f1a0151187b1c34c02934b6c3eb2
Saba Shavidze <sabashavidze@gmail.com>
2023-07-10 11:35:09 +0400

    NEW: supertest-setup testing library for http

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const compression = require('compression');
 const helmet = require('helmet');
+const request = require('supertest');
+
 const middlewares = require('./middlewares');
 
 const app = express();
@@ -12,10 +14,17 @@ app.use(express.json());// read json body
 
 app.get('/', (req, res) => {
   res.json({
-    message: '🌭🍾 H-Invetory API 🥊🌊',
+    message: '🌭 🍾 H-Invetory API 🥊 🌊',
   });
 });
-
+request(app)
+  .get('/')
+  .expect('Content-Type', /json/)
+  .expect('Content-Length', '48')
+  .expect(200)
+  .end((err, res) => {
+    if (err) console.log(err);
+  });
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
